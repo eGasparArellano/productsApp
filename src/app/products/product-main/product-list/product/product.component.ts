1ffadb0e814df458890919b41b5834a7ef5fe54f
Elias <eGasparArellano@gmail.com>
2020-04-12 21:40:07 -0500

    Changing absolute paths to relative paths of imports in order to fix the package error on Stackblitz

--- src/app/products/product-main/product-list/product/product.component.ts
+++ src/app/products/product-main/product-list/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Product } from 'src/app/products/Product';
+import { Product } from '../../../Product';
 import { Router } from '@angular/router';
 
 @Component({
